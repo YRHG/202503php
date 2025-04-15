3eb77ac19985c1056349363761697c92b1999286
Albert HAN <975816342hhz@gmail.com>
2025-04-15 20:23:10 +0900

    01.php

--- backend/01.php
+++ backend/01.php
@@ -1,1 +1,17 @@
 rade me
+PhpStorm 2024.3.5
+Build #PS-243.26053.13, built on March 12, 2025
+授权给 Trial User
+订阅有效期至 2025年5月8日。
+Runtime version: 21.0.6+8-b631.39 aarch64 (JCEF 122.1.9)
+VM: OpenJDK 64-Bit Server VM by JetBrains s.r.o.
+Toolkit: sun.lwawt.macosx.LWCToolkit
+macOS 15.4
+GC: G1 Young Generation, G1 Concurrent GC, G1 Old Generation
+Memory: 2048M
+Cores: 8
+Metal Rendering is ON
+Registry:
+  run.processes.with.pty=TRUE
+  ide.experimental.ui=true
+
