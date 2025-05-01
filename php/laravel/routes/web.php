4f5f969973c8c4cdca4d6eaf30a72448fa3a1e1f
YRHG <975816342hhz@gmail.com>
2025-05-01 16:26:57 +0900

    laravel  routes web index

--- php/laravel/routes/web.php
+++ php/laravel/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\TestsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/welcome', [TestsController::class, 'index'])->name('test.index');
+Route::get('/login', [TestsController::class, 'login'])->name('test.login');
